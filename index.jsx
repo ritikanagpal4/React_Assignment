c5bee86639e7c91ee1c8b6af18b52c9cb2f49655
ritikanagpal4 <ritika.nagpal4@gmail.com>
2017-03-10 10:33:35 +0530

    Update index.jsx

--- index.jsx
+++ index.jsx
@@ -15,31 +15,25 @@ class App extends React.Component {
 		};
 		this.aim = this.aim.bind(this);
 	}
-	// componentWillReceiveProps(nextProps) {
-	// 	console.log(nextProps);
-	// 	this.setState({data: nextProps})
-	// }
 	aim(data) {
 		console.log(data);
 		data = data
 		this.setState({newData: data, id: false})
 	}
-
 	render() {
-
 		console.log(this.state);
 		return (
 			<div className='container'>
 				<div className='col-md-8'>
-					<Header/> {data.map((person, i) => <TableRow key={i} data={person}/>)}
+					<Header/>
+					{data.map((person, i) => <TableRow key={i} data={person}/>)}
 				</div>
 				<div className='col-md-4'>
 					<AddPoll aim={this.aim}/>
 				</div>
 			</div>
 		);
-	}
-}
+  	}
+ }
 
-ReactDOM.render(
-	<App/>, document.getElementById('app'));
+ReactDOM.render(<App/>, document.getElementById('app'));
